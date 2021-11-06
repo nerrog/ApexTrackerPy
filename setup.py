f757b6e767db1cfb815ee5d13990f8b8ba8fd000
nerrog <tnk@nerrog.net>
2021-11-06 16:52:21 +0900

    add GetUIDbyName

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+from os import path
 from setuptools import setup, find_packages
 
 setup(
     name="ApexTrackerPy",
     author="nerrog",
     version="1.0",
+    url="https://github.com/nerrog/ApexTrackerPy",
+    long_description=open(('README.md'), encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     description="Library for easily calling Apex tracker APIs (supports TRN and APEX LEGENDS APIs)",
     install_requires=["requests"],
     packages=find_packages()
