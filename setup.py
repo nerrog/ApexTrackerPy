a1c2119c35ea823500cdf10ee7bdb90b8c76d742
nerrog <tnk@nerrog.net>
2021-11-06 17:01:43 +0900

    version 1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="ApexTrackerPy",
     author="nerrog",
-    version="1.0",
+    version="1.1",
     url="https://github.com/nerrog/ApexTrackerPy",
     long_description=open(('README.md'), encoding='utf-8').read(),
     long_description_content_type='text/markdown',
