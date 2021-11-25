cb0cc15b16688b4c1bebe4a3c12f5937c2a1e153
nerrog <tnk@nerrog.net>
2021-11-25 19:11:33 +0900

    Bug Fix (2)

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="ApexTrackerPy",
     author="nerrog",
-    version="1.8.1",
+    version="1.8.2",
     url="https://github.com/nerrog/ApexTrackerPy",
     long_description=open(('README.md'), encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     description="Library for easily calling Apex tracker APIs (supports TRN and APEX LEGENDS APIs)",
     install_requires=["requests"],
+    package_data={
+        "ApexTrackerPy": ["lang/*.json"],
+    },
     packages=find_packages()
 )
